98444a48d21276e2cd4148a9912d7e65bb07b018
ncicka <ncicka@gmail.com>
2024-07-30 18:30:06 -0300

    initial commit

---
+++ estructuras-control-iterativas.py
@@ -0,0 +1,23 @@
+# while
+# numero = 0
+# while (numero != 5):
+#     print("X", numero)
+#     numero = numero + 1
+
+# for
+# for x in range(1,10):
+#     print("el valor de x:",x)
+
+# conjuntos=(1,2,3,4,5,6,7,'34','123','123')
+# for x in conjuntos:
+#     print("el valor de x:",x)
+
+
+diccionario={'llave': 'valor'}
+for elemento, valor in diccionario.items():
+    print("el valor de diccionario:", elemento, valor)
+
+
+diccionario={'llave1': 'valor1'}
+for elemento in diccionario.values():
+    print("el valor de diccionario:", elemento)
